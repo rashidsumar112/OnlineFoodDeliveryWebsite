db0ebd8fa78d77ea042e759b00410aa616be7e06
rashidsumar112 <rashidsumar59@gmail.com>
2026-04-07 23:12:31 +0500

    Complete Project

--- backend/config/dbCon.js
+++ backend/config/dbCon.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
-//mongodb+srv://rashid:rashid12@cluster0.brkmchz.mongodb.net/?
+
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/food-del')
+    await mongoose.connect(process.env.MONGODB_URL_ATLAS)
     .then(()=>console.log("Db Connected"))
     .catch((err)=>console.log(err))
     
